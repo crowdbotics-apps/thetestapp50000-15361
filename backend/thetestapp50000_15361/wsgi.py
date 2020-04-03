55ad400446769f408c3577448a54d02983465827
Crowdbotics <team@crowdbotics.com>
2020-04-03 01:13:15 +0000

    Initial commit.

---
+++ backend/thetestapp50000_15361/wsgi.py
@@ -0,0 +1,16 @@
+"""
+WSGI config for thetestapp50000_15361 project.
+
+It exposes the WSGI callable as a module-level variable named ``application``.
+
+For more information on this file, see
+https://docs.djangoproject.com/en/2.2/howto/deployment/wsgi/
+"""
+
+import os
+
+from django.core.wsgi import get_wsgi_application
+
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'thetestapp50000_15361.settings')
+
+application = get_wsgi_application()
